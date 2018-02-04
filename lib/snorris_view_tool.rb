c87ffde434c4e727c477b9a3b61d7d55e28149bb
Seth Norris <sethnorris1@gmail.com>
2018-02-03 22:55:52 -0700

    DevTool Initial commit

---
+++ lib/snorris_view_tool.rb
@@ -0,0 +1,6 @@
+require "snorris_view_tool/version"
+require "snorris_view_tool/renderer"
+
+module SnorrisViewTool
+  # Your code goes here...
+end
---
+++ lib/snorris_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SnorrisViewTool
+  VERSION = "0.1.0"
+end
